704e715af419a7ae025888c0321742d9094cac9f
muneebrehman-17 <muneeb.rehman1754@gmail.com>
2026-03-05 21:19:34 +0500

    Updated The Js File
    Initial

--- script.js
+++ script.js
@@ -2,15 +2,13 @@ const body = document.body;
 const toggleBtn = document.getElementById('theme-toggle');
 const icon = toggleBtn.querySelector('i');
 
-// 1. Check Local Storage on Load
-// If user previously chose 'light', apply it immediately
+
 if (localStorage.getItem('theme') === 'light') {
     body.classList.add('light-theme');
     icon.classList.remove('fa-sun');
-    icon.classList.add('fa-moon'); // Switch icon to moon
+    icon.classList.add('fa-moon');
 }
 
-// 2. Toggle Function
 function toggleTheme() {
     body.classList.toggle('light-theme');
     
@@ -25,9 +23,8 @@ function toggleTheme() {
         icon.classList.remove('fa-moon');
         icon.classList.add('fa-sun');
     }
-    // 1. Initialize Lenis (Smooth Scroll)
     const lenis = new Lenis({
-        duration: 1.2, // The higher the value, the smoother the scroll
+        duration: 1.2, 
         easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
         direction: 'vertical',
         gestureDirection: 'vertical',
@@ -44,7 +41,7 @@ function toggleTheme() {
 
     requestAnimationFrame(raf);
 
-    // 2. Initialize AOS (Fade In Animations)
+  
     AOS.init({
         duration: 1000, // Animation lasts 1 second
         once: true,     // Animation happens only once
